fb4244be02b55e096187bde608995ac90f17cd43
wasif296 <wasifzahoor296@gmail.com>
2025-12-28 19:01:54 +0500

    paid ammount and remaining amount changes

--- models/customer.js
+++ models/customer.js
@@ -5,10 +5,12 @@ const CustomerSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     cnic: { type: String, required: true },
     model: { type: String, required: true },
-    emi: { type: String, required: true }, // Isay aap Receipt No ya EMI No dono keh sakte hain
+    emi: { type: String, required: true },
     date: { type: String, required: true },
-    price: { type: String, default: "0" }, // Mobile ki keemat
-    type: { type: String, enum: ['Purchase', 'Sale'], default: 'Purchase' } // Purchase ya Sale
+    price: { type: String, default: "0" },            // Total Bill
+    paidAmount: { type: String, default: "0" },       // Naya: Jitne paise mil gaye
+    remainingAmount: { type: String, default: "0" },  // Naya: Jo baqi reh gaye
+    type: { type: String, enum: ['Purchase', 'Sale'], default: 'Sale' }
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Customer', CustomerSchema);
