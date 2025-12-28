ece56de68db29b0c0d14f0fe030e38bf11b0879e
wasif296 <wasifzahoor296@gmail.com>
2025-12-28 18:23:23 +0500

    added sales purchase tabs and bill printing

--- models/customer.js
+++ models/customer.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 
 const CustomerSchema = new mongoose.Schema({
-    
     name: { type: String, required: true },
-    
     phone: { type: String, required: true },
-    
     cnic: { type: String, required: true },
-    
     model: { type: String, required: true },
-    
-    emi: { type: String, required: true },
-    
-    date: { type: String, required: true }
-
+    emi: { type: String, required: true }, // Isay aap Receipt No ya EMI No dono keh sakte hain
+    date: { type: String, required: true },
+    price: { type: String, default: "0" }, // Mobile ki keemat
+    type: { type: String, enum: ['Purchase', 'Sale'], default: 'Purchase' } // Purchase ya Sale
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Customer', CustomerSchema);
